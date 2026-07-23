17156a6c7a6f8388aed5e1a9756fdd730385fa53
mtosalim <matheusosalim@gmail.com>
2026-07-23 20:29:51 -0300

    file updates

--- olist_ecom2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("C:/Users/mathe/Desktop/olist_ecom/orders/olist_orders_dataset.csv", encoding="utf-8")
-
-#print(df.head())
-#print(df.info())
-
-df.notnull()
